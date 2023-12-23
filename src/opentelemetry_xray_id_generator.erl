edd3d2a997f5cd603f02d5fb5d46a0437bfb6eab
Jake Morrison <jake@cogini.com>
2023-12-23 13:01:10 -0600

    Doc

--- src/opentelemetry_xray_id_generator.erl
+++ src/opentelemetry_xray_id_generator.erl
@@ -2,16 +2,15 @@
 %%
 %% Generate trace_id and span_id compatible with AWS X-Ray.
 %%
-%% An X-Ray trace_id consists of three values separated by hyphens.
+%% The X-Ray trace_id is a 64-bit identifier which is unique within a single
+%% trace. The first 32 bits is the timestamp, the remaining 96 bits are random.
+%%
+%% It is serilized as three values separated by hyphens.
 %% For example, the trace_id {1-5759e988-bd862e3fe1be46a994272793} includes three
-%% values: the version number 1, the time of the original request in Unix epoch
-%% time, and finally a 96-bit identifier for the trace which is globally unique.
-%% OpenTelemetry trace_ids are integers, so we combine the time and the unique
-%% id into a single 128-bit value.
+%% values: version number 1, the time of the original request in Unix epoch
+%% time in hex, and finally a 96-bit identifier for the trace which is globally unique.
 %%
-%% X-Ray (sub)segment ID is a 64-bit identifier which is unique within a single
-%% trace, and consists of 16 hexadecimal digits.
-%%
+%% https://docs.aws.amazon.com/xray/latest/devguide/xray-api-sendingdata.html#xray-api-traceids
 %% https://docs.aws.amazon.com/xray/latest/devguide/xray-concepts.html
 %% https://aws-otel.github.io/docs/getting-started/x-ray#configuring-the-aws-x-ray-exporter
 %% @end
@@ -30,7 +29,7 @@
 
 -include_lib("opentelemetry_api/include/gradualizer.hrl").
 
-% @doc Generates a 128 bit random integer to use as a trace id.
+% @doc Generate 128-bit random integer to use as trace id.
 -spec generate_trace_id() -> opentelemetry:trace_id().
 generate_trace_id() ->
   Timestamp = opentelemetry:convert_timestamp(opentelemetry:timestamp(), second),
@@ -39,7 +38,7 @@ generate_trace_id() ->
   merge_trace_id(Timestamp, UniqueId).
 
 
-% @doc Generates a 64 bit random integer to use as a span id.
+% @doc Generate 64-bit random integer to use as a span id.
 -spec generate_span_id() -> opentelemetry:span_id().
 generate_span_id() ->
   % 2 shifted left by 63 == 2 ^ 64
